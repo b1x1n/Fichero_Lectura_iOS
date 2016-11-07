e87f50475ec7acc6f10a1d735bce8ce87d9d94d5
Alumno <alumno@alumnos-mac.local>
2016-11-07 09:05:27 +0100

    Initial Commit

---
+++ 04_Ficheros_Lectura/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  04_Ficheros_Lectura
+//
+//  Created by Alumno on 07/11/16.
+//  Copyright © 2016 Alumno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
