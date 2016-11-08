7fa4789d5eeb47ea156cf2e7a63f38c3c01994b0
Alumno <alumno@Alumnos-Mac.local>
2016-11-08 09:01:43 +0100

    first commit

--- 04_Ficheros_Lectura/ViewController.m
+++ 04_Ficheros_Lectura/ViewController.m
@@ -16,7 +16,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //Como accedemos al diccionario??
+    
+    //Buscamos la ruta de donde esta nuestro diccionario
+    NSString* ruta = [[NSBundle mainBundle]pathForResource:@"DatosPersonales" ofType:@"plist"];
+   
+    //Creamos un objeto de tipo diccionario en memoria a partir de la ruta fisica de donde esta nuestro plist
+    NSDictionary* datosPersonales = [[NSDictionary alloc]initWithContentsOfFile:ruta];
+    //Podemos trabajar de manera normal con este diccionario
+    NSString* nombre = (NSString*)[datosPersonales objectForKey:@"nombre"];
+    NSString* apellidos = (NSString*)[datosPersonales objectForKey:@"apellidos"];
+    NSNumber* edad = (NSNumber*)[datosPersonales objectForKey:@"edad"];
+    NSDate* date = (NSDate*)[datosPersonales objectForKey:@"fecha de nacimiento"];
+    NSArray* direccion = (NSArray*)[datosPersonales objectForKey:@"direccion"];
+    NSLog(@"%@ %@ %@ %@ %@ ",nombre,apellidos,edad,date,direccion);
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ README.md
@@ -0,0 +1,1 @@
+#Fichero de Lectura iOS
